6125b7a2f8a09a28c7cbe41c41194cf598681707
1583511688wow <132656910+1583511688wow@users.noreply.github.com>
2023-05-05 17:48:04 +0800

    Update LoginInterceptor.java

--- Backend/ljh-backend/src/main/java/com/ljh/project/utils/LoginInterceptor.java
+++ Backend/ljh-backend/src/main/java/com/ljh/project/utils/LoginInterceptor.java
@@ -12,9 +12,9 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         // 1.判断是否需要拦截（ThreadLocal中是否有用户）
-        if (UserHolder.getUser() == null) {
+       // if (UserHolder.getUser() == null) {
 
-            new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
+         //  throw new BusinessException(ErrorCode.NOT_LOGIN_ERROR);
         }
         // 有用户，则放行
         return true;
